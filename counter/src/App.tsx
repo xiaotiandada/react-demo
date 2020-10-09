aa7da90c10c6c953f74a17c919fe666d126453e3
xiaotiandada <952822399@qq.com>
2020-10-10 00:42:33 +0800

    feat: counter redux toolkit

--- counter/src/App.tsx
+++ counter/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Counter } from "./counter/Counter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Counter></Counter>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ counter/src/counter/Counter.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styles from './Counter.module.css';
+
+export function Counter() {
+    return (
+        <div>
+            <div className={styles.row}>
+                <button
+                    className={styles.button}
+                    aria-label="Increment value"
+                >
+                    +
+          </button>
+                <span className={styles.value}></span>
+                <button
+                    className={styles.button}
+                    aria-label="Decrement value"
+                >
+                    -
+          </button>
+            </div>
+            <div className={styles.row}>
+                <input
+                    className={styles.textbox}
+                    aria-label="Set increment amount"
+                />
+                <button
+                    className={styles.button}
+                >
+                    Add Amount
+          </button>
+                <button
+                    className={styles.asyncButton}
+                >
+                    Add Async
+          </button>
+            </div>
+        </div>
+    )
+}
